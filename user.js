39db4098bce7d496f283564217c9b18f362be4ad
Ravi Tharun <tr565003@gmail.com>
2026-07-19 14:16:55 +0530

    ji

--- user.js
+++ user.js
@@ -11,6 +11,15 @@ const user=()=>{
 console.log("Payment");
 
      alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
+     alert("Payment Module")
     
 
 }
