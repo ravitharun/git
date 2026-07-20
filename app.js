f9fc7119a5cfb0bf07de4780038e75e0f4e8afa9
Ravi Tharun <tr565003@gmail.com>
2026-07-20 07:52:30 +0530

    Updated the Function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // git revert -m 1 HEAD
+const GetUser = () => {
+
+
+    const UserLogin = true
+
+    if (UserLogin) {
+
+
+        return "User Login"
+    }
+
+
+    return "uer Not Login"
+}
+
+
+console.log(GetUser());
